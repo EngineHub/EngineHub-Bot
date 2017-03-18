5f6e441961069c764b01f7ecf6b52c4481d9b2b9
Madeline Miller <mnmiller1@me.com>
2017-03-18 19:31:35 +1000

    Fixed messages from NoSpam.

--- src/main/java/com/me4502/me4bot/discord/module/NoSpam.java
+++ src/main/java/com/me4502/me4bot/discord/module/NoSpam.java
@@ -22,15 +22,17 @@ public class NoSpam implements Module, EventListener {
             int mentionCount = content.length() - AT_PATTERN.matcher(content).replaceAll(Matcher.quoteReplacement("")).length();
             if (mentionCount >= 6 && !Me4Bot.isAuthorised(((MessageReceivedEvent) event).getAuthor())) {
                 ((MessageReceivedEvent) event).getMessage().delete().queue();
-                ((MessageReceivedEvent) event).getAuthor().getPrivateChannel().sendMessage("Oi mate, you seem to be spamming mentions! If you keep doing this you will be banned.").queue();
-                int spamTime = spamTimes.getOrDefault(((MessageReceivedEvent) event).getAuthor().getId(), 0);
-                spamTime ++;
-                spamTimes.put(((MessageReceivedEvent) event).getAuthor().getId(), spamTime);
-                if (spamTime >= 3) {
-                    // Do the ban.
-                    ((MessageReceivedEvent) event).getAuthor().getPrivateChannel().sendMessage("You have been banned for spamming. Contact Me4502#3758 if you believe this is a mistake.")
-                            .queue(message -> ((MessageReceivedEvent) event).getGuild().getController().ban(((MessageReceivedEvent) event).getAuthor(), 0).queue());
-                }
+                ((MessageReceivedEvent) event).getAuthor().openPrivateChannel().queue(privateChannel -> {
+                    privateChannel.sendMessage("Oi mate, you seem to be spamming mentions! If you keep doing this you will be banned.").queue();
+                    int spamTime = spamTimes.getOrDefault(((MessageReceivedEvent) event).getAuthor().getId(), 0);
+                    spamTime ++;
+                    spamTimes.put(((MessageReceivedEvent) event).getAuthor().getId(), spamTime);
+                    if (spamTime >= 3) {
+                        // Do the ban.
+                        privateChannel.sendMessage("You have been banned for spamming. Contact Me4502#3758 if you believe this is a mistake.")
+                                .queue(message -> ((MessageReceivedEvent) event).getGuild().getController().ban(((MessageReceivedEvent) event).getAuthor(), 0).queue());
+                    }
+                });
             } else {
                 spamTimes.remove(((MessageReceivedEvent) event).getAuthor().getId());
             }
