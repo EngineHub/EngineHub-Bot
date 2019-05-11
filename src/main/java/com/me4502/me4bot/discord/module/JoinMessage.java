cba44d60974892ebf3b8ca294708c61fab57b47b
Madeline Miller <mnmiller1@me.com>
2019-05-11 21:24:02 +1000

    Update Java

--- src/main/java/com/me4502/me4bot/discord/module/JoinMessage.java
+++ src/main/java/com/me4502/me4bot/discord/module/JoinMessage.java
@@ -22,28 +22,33 @@
 package com.me4502.me4bot.discord.module;
 
 import net.dv8tion.jda.core.MessageBuilder;
+import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.events.Event;
 import net.dv8tion.jda.core.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.core.hooks.EventListener;
 
 public class JoinMessage implements Module, EventListener {
 
+    private static Message message;
+
+    static {
+        MessageBuilder builder = new MessageBuilder();
+
+        builder.append("Hey! Welcome to the EngineHub Discord!\n\n");
+        builder.append("Before you get started, make sure you read the rules at <#139272202431234048>.\nwizjany, one of the "
+                + "developers, can be very helpful but also is angered easily. Make sure not to ping him, and don't mind him too "
+                + "much.\n\n");
+        builder.append("If you need help, put your server log `logs/latest.log` onto http://paste.enginehub.org and provide us with"
+                + " the link in the appropriate channel.\n\n~ The EngineHub team");
+        message = builder.build();
+    }
+
     @Override
     public void onEvent(Event event) {
         if (event instanceof GuildMemberJoinEvent) {
-            ((GuildMemberJoinEvent) event).getUser().openPrivateChannel().queue(
-                    privateChannel -> {
-                        MessageBuilder builder = new MessageBuilder();
-                        builder.append("Hey! Welcome to the EngineHub Discord!\n\n");
-                        builder.append("Before you get started, make sure you read the rules at <#139272202431234048>.\nwizjany, one of the "
-                                + "developers, can be very helpful but also is angered easily. Make sure not to ping him, and don't mind him too "
-                                + "much.\n\n");
-                        builder.append("If you need help, put your server log `logs/latest.log` onto http://paste.enginehub.org and provide us with"
-                                + " the link in the appropriate channel.\n\n~ The EngineHub team");
-                        privateChannel.sendMessage(
-                                builder.build()
-                        ).queue(message -> privateChannel.close().queue());
-                    });
+            ((GuildMemberJoinEvent) event).getUser().openPrivateChannel()
+                    .queue(privateChannel -> privateChannel.sendMessage(message)
+                            .queue(message -> privateChannel.close().queue()));
         }
     }
 
