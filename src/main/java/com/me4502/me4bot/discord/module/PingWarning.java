470d918013512676ba1018115c04b3b1a65c13ff
Madeline Miller <mnmiller1@me.com>
2020-01-21 21:46:46 +1000

    Kick then ban for dev ping spam

--- src/main/java/com/me4502/me4bot/discord/module/PingWarning.java
+++ src/main/java/com/me4502/me4bot/discord/module/PingWarning.java
@@ -22,13 +22,20 @@
 package com.me4502.me4bot.discord.module;
 
 import com.me4502.me4bot.discord.Me4Bot;
+import com.me4502.me4bot.discord.Settings;
 import com.me4502.me4bot.discord.util.PermissionRoles;
 import com.me4502.me4bot.discord.util.StringUtil;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.GenericEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.EventListener;
 
+import java.util.HashMap;
+
 public class PingWarning implements Module, EventListener {
+
+    private final HashMap<String, Integer> spamTimes = new HashMap<>();
 
     @Override
     public void onEvent(GenericEvent event) {
@@ -40,7 +47,27 @@ public class PingWarning implements Module, EventListener {
                 ((MessageReceivedEvent) event).getChannel().sendMessage("Hey " + StringUtil.annotateUser(
                         ((MessageReceivedEvent) event).getMember().getUser()
                 ) + "! It's against the rule to ping the developers, make sure to read the rules!").queue();
+                int spamTime = spamTimes.getOrDefault(((MessageReceivedEvent) event).getAuthor().getId(), 0);
+                spamTime ++;
+                spamTimes.put(((MessageReceivedEvent) event).getAuthor().getId(), spamTime);
+                if (spamTime >= 3) {
+                    // Do the ban.
+                    kickForSpam(((MessageReceivedEvent) event).getGuild(), ((MessageReceivedEvent) event).getAuthor(), spamTime >= 4);
+                }
             }
         }
+    }
+
+    public static void kickForSpam(Guild guild, User user, boolean ban) {
+        user.openPrivateChannel().queue((privateChannel -> {
+            privateChannel.sendMessage("You have been kicked for repeatedly pinging devs. Contact " + Settings.hostUsername + "#" + Settings.hostIdentifier + " if you believe this is a mistake.")
+                    .queue(message -> {
+                        if (ban) {
+                            guild.ban(user, 0, "Repeatedly pinging devs").queue();
+                        } else {
+                            guild.kick(guild.getMember(user), "Pinging devs").queue();
+                        }
+                    });
+        }));
     }
 }
