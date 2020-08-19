9bfbe5dbea1fb490438a7c7babc36aebd0eb7dce
Matthew Miller <mnmiller1@me.com>
2020-08-19 13:37:02 +1000

    Run license formatter

--- src/main/java/org/enginehub/discord/module/errorHelper/resolver/ErrorResolver.java
+++ src/main/java/org/enginehub/discord/module/errorHelper/resolver/ErrorResolver.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) Me4502 (Matthew Miller)
+ * Copyright (c) EngineHub and Contributors
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
