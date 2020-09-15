71e6ac124b37d8917b915ec4c5707a389664559d
Madeline Miller <mnmiller1@me.com>
2020-09-15 22:04:51 +1000

    Don't send a unknown command message

--- src/main/java/org/enginehub/discord/Settings.java
+++ src/main/java/org/enginehub/discord/Settings.java
@@ -93,6 +93,7 @@ public class Settings {
         ConfigurationLoader<CommentedConfigurationNode> loader = HoconConfigurationLoader.builder().setPath(new File("settings.conf").getAbsoluteFile().toPath()).build(); // Create the loader
         try {
             loadedNode.getNode("token").setValue(token);
+            // Using generic inference on the next line breaks javac. This is a load-bearing generic.
             loadedNode.getNode("auto-erase-channels").setValue(new TypeToken<List<String>>(){}, autoEraseChannels);
 
             loader.save(loadedNode);
