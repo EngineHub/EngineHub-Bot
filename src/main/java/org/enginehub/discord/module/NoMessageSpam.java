4569e81e56c6b02abb0298c6b94b4f198ad7f8e9
Octavia Togami <octavia.togami@gmail.com>
2021-10-23 18:03:55 -0700

    Merge with existing message spam module

--- src/main/java/org/enginehub/discord/module/NoMessageSpam.java
+++ src/main/java/org/enginehub/discord/module/NoMessageSpam.java
@@ -24,16 +24,24 @@ package org.enginehub.discord.module;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.events.message.priv.PrivateMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import ninja.leaping.configurate.ConfigurationNode;
 import org.enginehub.discord.EngineHubBot;
 import org.enginehub.discord.util.PermissionRoles;
 import org.enginehub.discord.util.PunishmentUtil;
+import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nonnull;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 /**
  * A simple anti-same-message spam filter.
@@ -67,6 +75,8 @@ public class NoMessageSpam extends ListenerAdapter implements Module {
     private final LoadingCache<CacheKey, AtomicInteger> messageCounts = CacheBuilder.newBuilder()
         .expireAfterAccess(1, TimeUnit.MINUTES)
         .build(CacheLoader.from(() -> new AtomicInteger(0)));
+    private final HashSet<Long> hasPingedBefore = new HashSet<>();
+    private volatile Set<Long> guildsForPunish;
 
     @Override
     public void onGuildMessageReceived(@Nonnull GuildMessageReceivedEvent event) {
@@ -87,5 +97,54 @@ public class NoMessageSpam extends ListenerAdapter implements Module {
         } else if (hashCount == 6) {
             PunishmentUtil.banUser(event.getGuild(), event.getAuthor(), "Message spam", true);
         }
+    }
+
+    @Override
+    public void onPrivateMessageReceived(@NotNull PrivateMessageReceivedEvent event) {
+        if (!(event.getMessage().mentionsEveryone() || event.getMessage().getContentRaw().contains("@everyone"))) {
+            return;
+        }
+
+        if (hasPingedBefore.add(event.getAuthor().getIdLong())) {
+            // They haven't pinged before, kick.
+            punishForAtEveryone(event, PunishmentUtil::kickUser);
+        } else {
+            // It's ban time.
+            punishForAtEveryone(
+                event,
+                (guild, member, reason) -> PunishmentUtil.banUser(guild, member.getUser(), reason, false)
+            );
+        }
+    }
+
+    // TODO: Consider extracting out?
+    private interface Punishment {
+        void enact(Guild guild, Member member, String reason);
+    }
+
+    private void punishForAtEveryone(@NotNull PrivateMessageReceivedEvent event, Punishment punishment) {
+        for (long guildId : guildsForPunish) {
+            Guild guild = event.getJDA().getGuildById(guildId);
+            if (guild == null) {
+                System.err.println("Warning, guild " + guildId + " does not appear to be known to this bot.");
+                continue;
+            }
+            Member member = guild.getMember(event.getAuthor());
+            if (member != null) {
+                punishment.enact(guild, member, "Attempting to ping everyone in DMs");
+            }
+        }
+    }
+
+    @Override
+    public void load(ConfigurationNode loadedNode) {
+        guildsForPunish = loadedNode.getNode("default-punishment-guilds").getChildrenList().stream()
+            .map(ConfigurationNode::getLong)
+            .collect(Collectors.toUnmodifiableSet());
+    }
+
+    @Override
+    public void save(ConfigurationNode loadedNode) {
+        loadedNode.getNode("default-punishment-guilds").setValue(guildsForPunish);
     }
 }
